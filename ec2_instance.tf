2d5457891848791d7e72029f178885edf77b7f10
Vibhuti Jain <jainvibhuti450@gmail.com>
2025-06-18 00:18:55 +0530

    for_each concept

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -46,21 +46,35 @@ resource "aws_security_group" "mysg"{
 
 #ec2 instance
 resource "aws_instance" "ec2_instance" {
+#   Meta Arguments
+    for_each = tomap ({
+     "EC2_instance_1" = "t2.micro",
+     "EC2_instance_2" = "t2.micro"
+    })
+
+#   Meta Arguments
+    depends_on = [ aws_key_pair.deployer, aws_security_group.mysg ]
+
     key_name = aws_key_pair.deployer.key_name
     security_groups = [aws_security_group.mysg.name]
-#   instance_type = "t2.micro"
-    instance_type = var.ec2_instance_type
-#   ami = "ami-0d1b5a8c13042c939"
-    ami = var.ec2_ami_id
+
+#   instance_type = "t2.micro"                -----> simple way
+#   instance_type = var.ec2_instance_type     -----> using variable
+    instance_type = each.value               #-----> for each syntax
+
+#   ami = "ami-0d1b5a8c13042c939"         ------> simple way
+    ami = var.ec2_ami_id                  #-----> using variable
     user_data = file("install_nginx.sh")
 
     root_block_device {
 #     volume_size = "8"
-      volume_size = var.ec2_root_storage_size
+#     volume_size = var.ec2_root_storage_size  ----> using variable
+      volume_size = var.env == "prd" ? 20 : var.ec2_root_default_storage_size    # ---> conditional expr
       volume_type = "gp3"
     }
   
     tags = {
-      Name = "Terraform-ec2"
+      Name = each.key                      #------> for_each syntax
+
     }
 }
